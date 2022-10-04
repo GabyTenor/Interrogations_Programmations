b67f17cb4842c777329e894d1aebbffdfa770d76
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-10-04 14:25:43 +0200

    Intérrogation sur les bases de POO, 04/10

---
+++ I1P622_Tarnus/I1P622_Tarnus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace I1P622_Tarnus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            trafficsLights[] lights = new trafficsLights[2]
+            {
+                new trafficsLights("Orange", 0001, "On"),
+                new trafficsLights("Green", 0002, "On")
+            };
+
+            string statusInfo;
+
+            Console.WriteLine("TRAFFIC LIGHT 1 :");
+            for(int compteur1 = 0; compteur1 < 6; compteur1++)
+            {
+                for (int i = 0; i < 3; i++)
+                { 
+                    statusInfo = lights[0].LightsStatus();
+                    Console.WriteLine(statusInfo);
+                    lights[0].ChangeColors(i);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TRAFFIC LIGHT 2 :");
+            for (int compteur2 = 0; compteur2 < 6; compteur2++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    lights[1].LightBlink(j);
+                    statusInfo = lights[1].LightsStatus();
+                    Console.WriteLine(statusInfo);
+                }
+            }
+                      
+            Console.ReadLine();
+        }
+    }
+}
---
+++ I1P622_Tarnus/I1P622_Tarnus/trafficsLights.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I1P622_Tarnus
+{
+    class trafficsLights
+    {
+        private string _color;
+        private int _number;
+        private string _status;
+        public trafficsLights(string _color, int _number, string _status)
+        {
+            this._color = _color;
+            this._number = _number;
+            this._status = _status;
+        }
+        public string LightsStatus()
+        {
+            string statusInfo = "The traffic light " + _number;
+
+            switch (_status)
+            {
+                case "On":
+                    statusInfo = statusInfo + " is on";
+                    break;
+
+                case "Off":
+                    statusInfo = statusInfo + " is off!!!!!";
+                    break;
+            }
+            
+            if(_status != "Off")
+            {
+                statusInfo = statusInfo + " and is showing the color " + _color;
+            }
+
+            return statusInfo;
+        }
+
+        public void ChangeColors(int i)
+        {     
+            switch (i)
+            {
+                case 0:
+                    _color = "Green";
+                    break;
+
+                case 1:
+                    _color = "Orange";
+                    break;
+
+                case 2:
+                    _color = "Red";
+                    break;
+            }           
+        }
+
+        public void LightBlink(int j)
+        {
+            if(j == 0)
+            {
+                _status = "On";
+            }
+            else if(j == 1)
+            {
+                _status = "Off";
+            }
+        }
+    }    
+}
